086baef542eb7722b942b1dde14ed52a6f6cad70
strikafragile <77917231+strikafragile@users.noreply.github.com>
2021-01-24 20:31:59 +0900

    Part2

--- practice1.js
+++ practice1.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express()
+const mongoose= require("mongoose");
+require("dotenv").config();
 
+//Import routes
+const postsRoute = require("./routes/posts");
+app.use("/posts",postsRoute);
+//Routes
 app.get("/",(req,res)=>{
-    res.send("Hello World")
+    res.send("Hello World") 
 });
 
-    app.listen(1000,function(){
+//Connect to Data Base
+mongoose.connect(process.env.DB_CONNECTS,
+    {useNewUrlParser:true, useUnifiedTopology:true},
+    ()=>console.log("connected to Date Base"));
+
+app.listen(1000,function(){
         console.log("ServerConnected")
     });
-
